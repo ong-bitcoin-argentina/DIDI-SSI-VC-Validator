f43bc4599c02821a605b487464c3d8940ffd35cf
Matias Oveja Smith <matias.ovejasmith@bitcoinargentina.org>
2021-12-15 11:16:10 -0300

    Fix: Export missing credentials

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,5 +1,6 @@
 export * as benefit from './benefit';
 export * as finance from './finance';
 export * as identity from './identity';
-// export * as livingPlace from './livingPlace';
-// export * as work from './work';
+export * as livingPlace from './livingPlace';
+export * as shareRequest from './shareRequest';
+export * as work from './work';
